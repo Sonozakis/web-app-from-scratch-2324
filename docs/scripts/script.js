67dbde2f64a53b85a2fd599267a2a470ce3e1b87
klaudia <102046293+Sonozakis@users.noreply.github.com>
2024-02-06 17:49:54 +0100

    Added comments to the JSON file

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -3,16 +3,21 @@ const jsonFile = "https://sonozakis.github.io/web-app-from-scratch-2324/docs/inf
 
   fetch(jsonFile) 
     .then((response) => {
+      // Check if the response is ok
       if(!response.ok) {
+        // If not show error
         throw new Error('Network response was not ok')
     }
+    // If it's ok return result from JSON file
     return response.json();
     })
     .then((data) => {
+      // Update the <p> in the HTML and show the JSON in the console
       nameFile.innerHTML = data.name;
       console.log(data);
     })
 
     .catch((error) => {
+      // If there is an error during fetching show error
       console.error("There is a problem fetching the JSON file", error);
   });
