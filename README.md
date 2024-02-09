1c784054cbdb7ef5831761e23944f51399abb0b0
klaudia <102046293+Sonozakis@users.noreply.github.com>
2024-02-09 01:50:31 +0100

    CSS, Javascript and HTML updates
    FInally decided on a design and made a basic CSS mobile layout for it. Made the first link functional with JS.

--- README.md
+++ README.md
@@ -7,8 +7,32 @@ Tijdens dag 1 heb ik vooral gekeken wat ik wilde qua concept. Ook snapte ik toen
 
 ## Dag 2 - 7 februari 2024
 Dag 2 heb ik vooral verder besteed aan het ontwerpen van mijn concept. Mijn eerste concepten hadden niet echt een microinteractie erin zitten, waardoor ik een beetje moeite had met kiezen wat ik wilde doen. Hierdoor heb ik een hoop schetsen gemaakt. Toen ik eindelijk had gekozen wat ik ongeveer wilde ben ik begonnen aan de mobiele basis layout van mijn CSS. 
-<img src="./readme-images/JSON-voorbeeld.png" alt="Mijn eerste JSON file.">
-<img src="./readme-images/JSON-voorbeeld.png" alt="Mijn eerste JSON file.">
+<img src="./readme-images/design1.png" alt="Mijn eerste design.">
+<img src="./readme-images/design2.png" alt="Mijn tweede design.">
+
+FOTO'S SCHETSEN TOEVOEGEN!!!!!!
 
 
 ## Dag 3 - 8 februari 2024
+Na een nachtje slapen vond ik mijn layout toch niks en ben ik weer opnieuw begonnen (ja ik word ook gek van mezelf). Deze keer was het wel snel raak en ben ik meteen begonnen aan de CSS. Ik heb mijn mobiele layout nu grotendeels af. Ook heb ik de eerste kleine functionaliteit erin verwerkt, namelijk het openen en sluiten van de informatie vakjes. 
+<img src="./readme-images/design3.png" alt="Mijn uiteindelijke design.">
+<img src="./readme-images/design3vak.png" alt="Screenshot met het vakje open.">
+
+Deze dag heb ik ook mijn aantekeningen netjes gemaakt van de afgelopen dagen, deze zijn te vinden op mijn [Notion](https://obvious-seal-dd9.notion.site/Minor-Web-Development-6e526033f6244a08af249c72653b22b8?pvs=4).
+
+## Dag 4 - 9 februari 2024
+To do:
+- Mobiele layout compleet af
+- Tekst schrijven
+- Beginnen aan middel en grote scherm versies
+
+
+## Dag 5 - 10 februari 2024
+To do:
+- Middel en grote versies af
+- Animaties microinteracties mooi maken
+- Easter egg toevoegen?
+
+
+Bronnen:
+- https://www.peakpx.com/en/hd-wallpaper-desktop-fjegq
--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,23 +1,39 @@
-const nameFile = document.querySelector("");
-const jsonFile = "https://sonozakis.github.io/web-app-from-scratch-2324/docs/info.json";
+// const nameFile = document.querySelector("");
+// const jsonFile = "https://sonozakis.github.io/web-app-from-scratch-2324/docs/scripts/info.json";
 
-  fetch(jsonFile) 
-    .then((response) => {
-      // Check if the response is ok
-      if(!response.ok) {
-        // If not show error
-        throw new Error('Network response was not ok')
-    }
-    // If it's ok return result from JSON file
-    return response.json();
-    })
-    .then((data) => {
-      // Update the <p> in the HTML and show the JSON in the console
-      nameFile.innerHTML = data.name;
-      console.log(data);
-    })
+//   fetch(jsonFile) 
+//     .then((response) => {
+//       // Check if the response is ok
+//       if(!response.ok) {
+//         // If not show error
+//         throw new Error('Network response was not ok')
+//     }
+//     // If it's ok return result from JSON file
+//     return response.json();
+//     })
+//     .then((data) => {
+//       // Update the <p> in the HTML and show the JSON in the console
+//       nameFile.innerHTML = data.name;
+//       console.log(data);
+//     })
 
-    .catch((error) => {
-      // If there is an error during fetching show error
-      console.error("There is a problem fetching the JSON file", error);
-  });
+//     .catch((error) => {
+//       // If there is an error during fetching show error
+//       console.error("There is a problem fetching the JSON file", error);
+//   });
+
+
+const showAbout = document.querySelector('header section ul li:nth-of-type(1) a');
+const about = document.querySelector('main article');
+    
+// Event listener that opens article on click
+document.addEventListener('click', function(event) {
+  if (event.target === showAbout) {
+    // If user clicks link
+    event.preventDefault();
+    about.classList.add('show'); // Adds the 'show' class
+  } else if (event.target = getComputedStyle(about, '::after')) {
+    // If user clicks on the close button 
+    about.classList.remove('show'); // Removes the 'show' class
+  }
+});
