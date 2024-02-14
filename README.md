61de32402201d4720bdb6a3194ba12417a5badd8
klaudia <102046293+Sonozakis@users.noreply.github.com>
2024-02-14 23:51:48 +0100

    Updates to everything
    Finished the website/app.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Web App From Scratch @cmda-minor-web 2023 - 2024
+## Idee
+Mijn uiteindelijke idee was een mix tussen cybercore en mijn eigen style. Zo ben ik gekomen op het Windows-achtige concept. Ik heb veel blauw gebruikt, omdat ik deze kleur associeer met technologie. Daarnaast heb ik wat filters gegooid op alle afbeeldingen zodat deze een pixelachtig effect zouden krijgen en zo ook goed bij elkaar zouden passen. Door de vakjes met de content ook een beetje willekeurig te positioneren op het scherm, wilde ik het Windows idee verder naar voren laten komen. Hierbij heb ik ook geprobeerd om bijpassende animaties te maken.
 
-## Documentatie
-**thema, idee, code, breakdown schets**
-
+## Breakdownschets
+Om mijn design goed te kunnen uitvoeren en om het een beetje overzichtelijk te houden voor mezelf heb ik een breakdownschets gemaakt.
+<img src="./readme-images/breakdownschets.png" alt="Mijn breakdown schets">
 
 ## Proces
 ### Dag 1 - 6 februari 2024
@@ -13,10 +15,12 @@ Tijdens dag 1 heb ik vooral gekeken wat ik wilde qua concept. Ook snapte ik toen
 
 ### Dag 2 - 7 februari 2024
 Dag 2 heb ik vooral verder besteed aan het ontwerpen van mijn concept. Mijn eerste concepten hadden niet echt een microinteractie erin zitten, waardoor ik een beetje moeite had met kiezen wat ik wilde doen. Hierdoor heb ik een hoop schetsen gemaakt. Toen ik eindelijk had gekozen wat ik ongeveer wilde ben ik begonnen aan de mobiele basis layout van mijn CSS. 
+<img src="./readme-images/schets1.jpg" alt="Schets 1.">
+<img src="./readme-images/schets2.jpg" alt="Schets 2.">
+<img src="./readme-images/schets3.jpg" alt="Schets 3.">
+<img src="./readme-images/schets4.png" alt="Schets 4.">
 <img src="./readme-images/design1.png" alt="Mijn eerste design.">
 <img src="./readme-images/design2.png" alt="Mijn tweede design.">
-
-**FOTO'S SCHETSEN TOEVOEGEN!!!!!!**
 
 
 ### Dag 3 - 8 februari 2024
@@ -58,7 +62,7 @@ Als laatste heb ik ook mijn JSON geupdated met de informatie die de docenten nod
 
 ### Dag 6 - 13 februari 2024
 Ik voelde mij niet zo lekker vandaag, dus heb ik op school best weinig gedaan. Als eerste heb ik de scrollbar aangepast, omdat de standaard scrollbar er niet mooi uitzag binnen mijn layout. 
-**FOTO TOEVOEGEN**
+<img src="./readme-images/scrollbar.png" alt="De nieuwe scrollbar">
 
 Het volgende wat ik heb gedaan is wat informatie plaatsen binnen de sources pagina, zodat ik een basis heb. Hier komen later nog meer sources bij.
 
@@ -70,26 +74,47 @@ Na een dutje te nemen thuis ben ik hard aan de slag gegaan met mijn website afma
 
 Omdat ik hier een beetje gek van werd en ik niet goed wist hoe ik dit in godsnaam moest oplossen, heb ik besloten om dit te laten gaan en gewoon een eigen JSON file op te roepen met afbeeldingen en titels van al mijn favorieten. Dit is uiteindelijk heel soepel gegaan door middel van de uitleg die wij vorige week kregen van Marten over JSON en deze files fetchen. 
 
-**FOTO TOEVOEGEN**
+<img src="./readme-images/JSON-api.png" alt="Mijn JSON bestand.">
 
-Hierna heb ik alle favorieten in 3 verschillende carrousels gestopt. Ook heb ik nog even de responsiveness van de vakjes netjes gemaakt.
+Hierna heb ik alle favorieten in 3 verschillende carousels gestopt. Ook heb ik nog even de responsiveness van de vakjes netjes gemaakt.
 
-**FOTO TOEVOEGEN**
+<img src="./readme-images/carrousel.png" alt="De carousels.">
 
 Als laatste heb ik animaties gemaakt voor het open- en dichtmaken van de subpagina's. Ook heb ik een kleine animatie toegevoegd aan het menu en daar een geluidje aan toegevoegd.
 
-**FOTO TOEVOEGEN**
-
-- Favs erin zetten
-    - Api connecten
-    - Carrousel per genre?
-- Responsiveness favs
-- Vakjes anders
-- Animatie header (+ geluid?)
-- Animaties main
-
 ### Dag 7 - 14 februari 2024
-- Foto's schetsen toevoegen -> puntjes op de i van de readme
-- Breakdown schets iPad
-- Documentatie
+De laatste dag heb ik nog wat code stukjes aangepast waar ik niet tevreden mee was, mijn CSS een beetje opgeruimd en nog wat extra comments toegevoegd aan mijn JavaScript zodat ik het beter zou snappen. Ook heb ik alle gebruikte afbeeldingen op de pagina geedit zodat ze meer zouden passen bij het thema van de website.
 
+Ook probeerde ik nog mijn JavaScript te optimaliseren, maar toen ik dat deed viel alles uit elkaar. Hierdoor besloot ik het maar om het te laten en de opdracht zo in te leveren.
+
+<img src="./readme-images/javascriptfoutje.png" alt="Resultaten van mijn klooien met Javascript.">
+
+# Bronnen
+## Code
+- Font stack: https://modernfontstacks.com/
+- Fetch basis: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+- Carousel: https://www.educative.io/answers/how-to-implement-a-carousel-slider-using-javascript, https://dev.to/jvon1904/how-to-wrap-around-a-range-of-numbers-with-the-modulo-cdo
+
+## Informatie
+- De gebruikte informatie bij de "favorites" is geschreven door ChatGPT.
+
+## Geluid
+- https://www.youtube.com/watch?v=kBNOZdXwzdg
+
+## Afbeeldingen
+- https://mangaterial.tumblr.com/post/617046609171267584/rei-ayanamimanga-icons-pls-like-if-you-save
+- https://m.media-amazon.com/images/M/MV5BODZkZjUxNmEtMGEyOS00ZDY5LTkxZDMtZTJkZDBiZTkyOWRkXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg
+- https://static.wikia.nocookie.net/umineko/images/f/f6/Higurashi_2006_Anime_Cover.jpg/revision/latest?cb=20200709041329
+- https://m.media-amazon.com/images/M/MV5BNjdhYzY0M2QtODgwZi00NDg2LTliNDQtMzA4ZTMzMzQ0MGEyXkEyXkFqcGdeQXVyMTA3OTEyODI1._V1_.jpg
+- https://m.media-amazon.com/images/M/MV5BMzRjNDRlYjItYmRmMy00ZmExLWFlYzgtN2QyYjlmYmQ3MjI3XkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg
+- https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/922742d9acaeba7d887ed11b6caab0e4.jpe
+- https://e.snmc.io/lk/l/x/8efa2ae3eb2542c053ace125ce939c3c/10667276
+- https://www.rpgfan.com/wp-content/uploads/2020/10/Shin-Megami-Tensei-Persona-3-Artwork-023-scaled.jpg
+- https://upload.wikimedia.org/wikipedia/en/8/89/Dragon_Age_Origins_cover.png
+- https://m.media-amazon.com/images/I/61hp8zM2D4L._AC_UF894,1000_QL80_.jpg
+- https://assets.vg247.com/current//2018/05/red_dead_redemption_2_cover_art_1.jpg
+- https://www.interviewmagazine.com/wp-content/uploads/2023/02/7_NEW.jpg
+- https://ca-times.brightspotcdn.com/dims4/default/0c0e780/2147483647/strip/false/crop/2662x4000+0+0/resize/989x1486!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F06%2F10%2Ff4afc21d481f977446b76eac8839%2Fby-luis-panch-perez.jpg
+- https://i.pinimg.com/564x/c4/1f/35/c41f356a55be8a252ce51511a8f6c2cc.jpg
+- https://img.parool.nl/944168d23b88e156462d93a28d8f40d4fe8f9256/het-album-van-rb-zangeres-kali-uchis-staat-in-het-teken-van-seks-maar-dan-wel-de-luxe-variant-daarvan
+- https://lastfm.freetls.fastly.net/i/u/ar0/38a7155c834f80a49df821a741a68b17.jpg
--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,67 +1,158 @@
+// CLICKABLE WINDOWS //
 const showAbout = document.querySelector('header section ul li:nth-of-type(1) a');
 const about = document.querySelector('main article:nth-of-type(1)');
 const showFavs = document.querySelector('header section ul li:nth-of-type(2) a');
-const favs = document.querySelector('main article:nth-of-type(2)');
-const showSources = document.querySelector('header section ul li:nth-of-type(3) a');
-const sources = document.querySelector('main article:nth-of-type(3)');
-    
+const favs = document.querySelectorAll('main > div > article');
+
 // Event listener that opens article on click
 document.addEventListener('click', function(event) {
   if (event.target === showAbout) {
-    // If user clicks link
+    event.preventDefault(); // Prevents the default action of a
+    about.classList.add('show'); // Opens the article
+    showWindow(about); // Adds window animation
+  } 
+  else if (event.target === showFavs) {
     event.preventDefault();
-    about.classList.add('show'); // Adds the 'show' class
-  } else if (event.target.nodeName === 'BUTTON') {
-    // If user clicks on a button
-    about.classList.remove('show'); // Removes the 'show' class from about
+    favs.forEach(article => {
+      article.classList.add('show');
+      showWindow(article); 
+    });
+  } 
+  // Checks if the clicked button is the x button
+  else if (event.target.closest('main > article > button') || event.target.closest('main > div > article > button')) {
+    about.classList.remove('show'); // Removes the 'show' class from 'about'
+    // Removes the 'show' class from each article inside 'favs' section
+    favs.forEach(article => article.classList.remove('show'));
   }
 });
 
-document.addEventListener('click', function(event) {
-  if (event.target === showFavs) {
-    event.preventDefault();
-    favs.classList.add('show'); 
-  } else if (event.target.nodeName === 'BUTTON') {
-    favs.classList.remove('show'); 
-  }
-});
+// Function to add the window animation
+function showWindow(element) {
+  element.classList.add('window'); // Adds the 'window' class to element
+  // When animation ends remove class
+  element.addEventListener('animationend', () => {
+    element.classList.remove('window');
+  });
+}
 
-document.addEventListener('click', function(event) {
-  if (event.target === showSources) {
-    event.preventDefault();
-    sources.classList.add('show'); 
-  } else if (event.target.nodeName === 'BUTTON') {
-    sources.classList.remove('show'); 
-  }
-});
 
-// Load data from favorites.json and populate HTML elements
+
+// JSON //
+// Preloads everything
 function loadData() {
-  // Fetch the JSON data from favorites.json
   fetch('https://sonozakis.github.io/web-app-from-scratch-2324/scripts/favorites.json')
       .then(response => response.json())
       .then(data => {
-          // Populate HTML elements with anime data
+          // Anime data
           document.querySelector('main div article:nth-of-type(1) div img').src = data.anime[0].image;
           document.querySelector('main div article:nth-of-type(1) div section h3').textContent = data.anime[0].name;
           document.querySelector('main div article:nth-of-type(1) div section p:nth-of-type(1)').textContent = `${data.anime[0].year}`;
           document.querySelector('main div article:nth-of-type(1) div section p:nth-of-type(2)').textContent = `${data.anime[0].information}`;
 
-          // Populate HTML elements with game data
+          // Game data
           document.querySelector('main div article:nth-of-type(2) div img').src = data.games[0].image;
           document.querySelector('main div article:nth-of-type(2) div section h3').textContent = data.games[0].name;
           document.querySelector('main div article:nth-of-type(2) div section p:nth-of-type(1)').textContent = `${data.games[0].year}`;
           document.querySelector('main div article:nth-of-type(2) div section p:nth-of-type(2)').textContent = `${data.games[0].information}`;
 
-          // Populate HTML elements with music data
-          document.querySelector('main div article:nth-of-type(3) div img').src = data.music[0].image;
-          document.querySelector('main div article:nth-of-type(3) div section h3').textContent = data.music[0].name;
-          document.querySelector('main div article:nth-of-type(3) div section p').textContent = `${data.music[0].information}`;
+          // Music data
+          document.querySelector('main > div article:nth-of-type(3) div img').src = data.music[0].image;
+          document.querySelector('main > div article:nth-of-type(3) div section h3').textContent = data.music[0].name;
+          document.querySelector('main > div article:nth-of-type(3) div section p').textContent = `${data.music[0].information}`;
       })
+
+      // Error when JSON not loaded properly
       .catch(error => {
           console.error('Error loading favorites:', error);
       });
 }
 
-// Call the function to load data when the window finishes loading
+// Only loads data when web page is fully loaded
 window.addEventListener('load', loadData);
+
+// Stores index of each carousel
+let animeIndex = 0;
+let gamesIndex = 0;
+let musicIndex = 0;
+
+// Function to handle carousel navigation
+function navigateCarousel(type, direction) {
+  if (type === 'anime') {
+    animeIndex = (animeIndex + direction + 5) % 5; // Used to loop carousel
+    fetchAndLoadData();
+  } else if (type === 'games') {
+    gamesIndex = (gamesIndex + direction + 5) % 5; 
+    fetchAndLoadData();
+  } else if (type === 'music') {
+    musicIndex = (musicIndex + direction + 5) % 5; 
+    fetchAndLoadData();
+  }
+}
+
+// Loads data into the carousel
+function fetchAndLoadData() {
+  fetch('https://sonozakis.github.io/web-app-from-scratch-2324/scripts/favorites.json')
+    .then(response => response.json())
+    .then(data => {
+      // Updates html
+      updateAnime(data.anime[animeIndex]);
+      updateGames(data.games[gamesIndex]);
+      updateMusic(data.music[musicIndex]);
+    })
+    .catch(error => {
+      console.error('Error loading favorites:', error);
+    });
+}
+
+// Updates anime data
+function updateAnime(anime) {
+  document.querySelector('main div article:nth-of-type(1) div img').src = anime.image;
+  document.querySelector('main div article:nth-of-type(1) div section h3').textContent = anime.name;
+  document.querySelector('main div article:nth-of-type(1) div section p:nth-of-type(1)').textContent = `${anime.year}`;
+  document.querySelector('main div article:nth-of-type(1) div section p:nth-of-type(2)').textContent = `${anime.information}`;
+}
+
+// Updates games data
+function updateGames(game) {
+  document.querySelector('main div article:nth-of-type(2) div img').src = game.image;
+  document.querySelector('main div article:nth-of-type(2) div section h3').textContent = game.name;
+  document.querySelector('main div article:nth-of-type(2) div section p:nth-of-type(1)').textContent = `${game.year}`;
+  document.querySelector('main div article:nth-of-type(2) div section p:nth-of-type(2)').textContent = `${game.information}`;
+}
+
+// Updates music data
+function updateMusic(music) {
+  document.querySelector('main > div article:nth-of-type(3) div img').src = music.image;
+  document.querySelector('main > div article:nth-of-type(3) div section h3').textContent = music.name;
+  document.querySelector('main > div article:nth-of-type(3) div section p').textContent = `${music.information}`;
+}
+
+// Previous and next buttons for each article
+document.querySelector('main div article:nth-of-type(1) div:nth-of-type(2) button:nth-of-type(1)').addEventListener('click', () => navigateCarousel('anime', -1));
+document.querySelector('main div article:nth-of-type(1) div:nth-of-type(2) button:nth-of-type(2)').addEventListener('click', () => navigateCarousel('anime', 1));
+
+document.querySelector('main div article:nth-of-type(2) div:nth-of-type(2) button:nth-of-type(1)').addEventListener('click', () => navigateCarousel('games', -1));
+document.querySelector('main div article:nth-of-type(2) div:nth-of-type(2) button:nth-of-type(2)').addEventListener('click', () => navigateCarousel('games', 1));
+
+document.querySelector('main div article:nth-of-type(3) div:nth-of-type(2) button:nth-of-type(1)').addEventListener('click', () => navigateCarousel('music', -1));
+document.querySelector('main div article:nth-of-type(3) div:nth-of-type(2) button:nth-of-type(2)').addEventListener('click', () => navigateCarousel('music', 1));
+
+
+
+// ANIMATIONS //
+// Error sound
+const errorSound = new Audio('./sounds/errorsound.mp3');
+
+// Listens for error button click
+document.querySelector('header section button').addEventListener('click', function() {
+    errorSound.play();
+
+  setTimeout(() => {
+    document.querySelector('header section').classList.add('shake');
+
+    // Removes the shake class after the animation ends
+    setTimeout(() => {
+      document.querySelector('header section').classList.remove('shake');
+    }, 500); //
+  }, 100); // 100ms is the delay to match the sound
+});
